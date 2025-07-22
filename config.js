c508abe25b88892a1aaf1bb9d7723837cb019bc9
Aditi Balaji <aditiblji.official6634@gmail.com>
2025-07-22 18:16:16 +0530

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// Configuration file for API keys and other sensitive data
-const config = {
-    apiKey: "YOUR_OPENWEATHER_API_KEY", // Replace with your actual API key
-  };
-  
-  // Export the config object
-  export default config;
